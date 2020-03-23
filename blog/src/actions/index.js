188bbfc2a8035a8beda8b18587abda74d612d845
chong rui <raygohcr@gmail.com>
2020-03-23 22:38:11 +0800

    Changes toindex and PostList

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,23 @@
 import jsonPlaceHolder from "../apis/JsonPlaceHolder";
 import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  //dispatch a inner function to redux to invoke it
+  //using await to wait for the response of the API to arrive
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  //no need to put await as there is no logic after the line, we dont care about how long the user takes to return
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  //(extra codes)
+  //getState().posts is passed into map behind the scene
+  // _.chain(getState().posts)
+  //   .map("userId")
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value();
+};
+
 export const fetchPosts = () => {
   return async dispatch => {
     const res = await jsonPlaceHolder.get("/posts");
@@ -11,17 +29,24 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = id => {
-  return dispatch => {
-    _fetchUser(id, dispatch);
-  };
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const res = await jsonPlaceHolder.get(`/users/${id}`);
-
   dispatch({
     type: "FETCH_USER",
     payload: res.data
   });
-});
+};
+// export const fetchUser = id => {
+//   return dispatch => {
+//     _fetchUser(id, dispatch);
+//   };
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const res = await jsonPlaceHolder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: res.data
+//   });
+// });

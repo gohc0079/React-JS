6680bb34a317ffa4600a32d51312cc2c053ed02e
chong rui <raygohcr@gmail.com>
2020-04-02 22:15:28 +0800

    Added codes for redux chrome debugger

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+//for redux store debugger. WIth this debugger we can more easily see whats inside the redux store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
